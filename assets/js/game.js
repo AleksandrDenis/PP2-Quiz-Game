a02ae817be7f9f2b12e11c43a227d71e335dd3ef
Denis Kudriavcev <deniskudriavcev@gmail.com>
2024-02-25 14:11:02 +0000

    Add initial structure for game.js

---
+++ assets/js/game.js
@@ -0,0 +1,20 @@
+// Select HTML elements
+
+// Variables
+
+// Questions array
+
+// Function to display questions
+
+// Function to decrement timer
+
+// Function to check answers
+
+// Function to mark correct answer
+
+// Function to mark incorrect answer
+
+// Function to go to next question
+
+// Function to end game
+
