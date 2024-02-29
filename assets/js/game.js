dcf42734537994a8777c63fbf8de93a631341b88
Denis Kudriavcev <deniskudriavcev@gmail.com>
2024-02-29 21:15:13 +0000

    Add basic stfunctions that structure game

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,16 +5,37 @@
 // Questions array
 
 // Function to display questions
+function displayQuestions() {
+
+}
 
 // Function to decrement timer
+function decrementTimer() {
+
+}
 
 // Function to check answers
+function checkAnswers() {
+
+}
 
 // Function to mark correct answer
+function markCorrect() {
+
+}
 
 // Function to mark incorrect answer
+function markIncorrect() {
+
+}
 
 // Function to go to next question
+function nextQuestion() {
+
+}
 
 // Function to end game
+function endGame() {
+
+}
 
