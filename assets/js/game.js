3f0df0033e3610edf25168636cc5bf059f567bbf
Denis Kudriavcev <deniskudriavcev@gmail.com>
2024-02-29 21:28:24 +0000

    Update game.js with HTML element selections

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,15 @@
-// Select HTML elements
+// Select HTML elements in DOM
+const quizArea = document.getElementById('quiz-area');
+const scoreArea = document.getElementById('score-area');
+const timer = document.getElementById('timer');
+const timerBar = document.getElementById('timer-bar');
+const timerProgress = document.getElementById('timer-progress');
+const question = document.getElementById('question');
+const questionImage = document.getElementById('question-image');
+const answersChoices = document.getElementById('answers-choices');
+const choiceA = document.getElementById('A');
+const choiceB = document.getElementById('B');
+const choiceC = document.getElementById('C');
 
 // Variables
 
